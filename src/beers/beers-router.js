b2cac7431db424ada4abc70ec8380e15e9ec94a0
jakep5 <jakepagel1@yahoo.com>
2019-11-25 16:00:18 -0600

    add getUserBeers endpoint

---
+++ src/beers/beers-router.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const BeersService = require('./beers-service')
+const { requireAuth } = require('../middleware/jwt-auth')
+
+const beersRouter = express.Router()
+
+beersRouter
+    .route('/:user_id')
+    .all(requireAuth)
+    .get((req, res, next) => {
+        BeersService.getUserBeers(
+            req.app.get('db'),
+            req.params.user_id
+        )
+        .then(beers => {
+            console.log(beers)
+            res.json(beers.map(BeersService.serializeBeer))
+        })
+        .catch(next)
+})
+
+module.exports = beersRouter
---
+++ src/beers/beers-service.js
@@ -0,0 +1,36 @@
+const xss = require('xss')
+
+const BeersService = {
+    getAllBeers(db) {
+        return db
+            .from('craft-digest_beers AS beer')
+            .select(
+                'beer.id',
+                'beer.name',
+                'beer.brewery_name',
+                'beer.brewery_location',
+                'beer.tasting_notes',
+                'beer.abv',
+                'beer.rating'
+            )
+    },
+
+    getUserBeers(db, userId) {
+        return BeersService.getAllBeers(db)
+        .where('user_id', userId)
+    },
+
+    serializeBeer(beer) {
+        return {
+            id: beer.id,
+            name: xss(beer.name),
+            brewery_name: xss(beer.brewery_name),
+            brewery_location: xss(beer.brewery_location),
+            tasting_notes: xss(beer.tasting_notes),
+            abv: beer.abv,
+            rating: beer.rating
+        }
+    }
+}
+
+module.exports = BeersService
